1f865229394eb413ce7046d0ac3b3461434e52e4
stewart borle <stewi1014@gmail.com>
2024-07-12 20:25:50 +0800

    Create hello world GTK4 app

---
+++ src/pwnode.c
@@ -0,0 +1,34 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+
+#include "pwnode.h"
+
+int
+main (int argc, char *argv[])
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("com.github.stewi1014.pwnode", 0);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
+
+static void
+activate (GtkApplication *app, gpointer user_data)
+{
+  GtkBuilder *builder;
+  GtkWindow *window;
+
+  builder = gtk_builder_new_from_resource (
+      "/com/github/stewi1014/pwnode/window.ui");
+  window = GTK_WINDOW (gtk_builder_get_object (builder, "window"));
+
+  gtk_window_set_application (window, app);
+  gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
+
+  g_object_unref (builder);
+}
---
+++ src/pwnode.h
@@ -0,0 +1,9 @@
+#ifndef PWNODE_H
+#define PWNODE_H
+
+#include <gtk/gtk.h>
+
+int main (int argc, char *argv[]);
+static void activate (GtkApplication *app, gpointer user_data);
+
+#endif
